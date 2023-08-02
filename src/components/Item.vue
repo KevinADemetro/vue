67da473b47e9e6f21d72317fe0975688bb33d597
Kevin Albino Demetro <albinodemetrok@gmail.com>
2023-08-01 21:37:03 -0300

    Criando componente de item e página de listagem de itens

---
+++ src/components/Item.vue
@@ -0,0 +1,19 @@
+<script setup>
+    defineProps(['title'])
+</script>
+<template>
+    <div class="container">
+        <div>
+            <h1>{{ title }}</h1>
+            <p>Lorem ipsum dolor sit amet, <br/>
+                consectetur adipiscing elit.</p>
+        </div>
+        <button class="btn fill">BUY NOW</button>
+    </div>
+</template>
+
+<style scoped>
+.container{
+    border: 1px solid red;
+}
+</style>
---
+++ src/views/ItemsListView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Item from "../components/Item.vue";
+</script>
+
+<template>
+    <div class="container">
+        <h1>Items List</h1>
+        <Item 
+            title="teste"
+        />
+    </div>
+</template>
+<style scoped>
+</style>
